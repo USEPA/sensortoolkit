17eef1fc86ebeb0b511db16666ac0ef9a84d1a96
Samuel Frederick <frederick.samuel@epa.gov>
2021-05-19 11:04:03 -0400

    Add API credential examples, change instantiation name to Eval

--- Data and Figures/example_evaluation.py
+++ Data and Figures/example_evaluation.py
@@ -28,16 +28,16 @@ ref_path = pathlib.PureWindowsPath(ref_path)
 #  ----------------------------------------------------------------------------
 # Uncomment the following block of code to run pre-processing on downloaded
 # AirNowTech data.
-"""
+
 # Pre-process downloaded AirNowTech file, create separate, monthly files for
 # PM, gas, and met
 airnowtech_path = (ref_path.as_posix() + '/airnowtech/downloaded_datasets/' +
                    'AirNowTech_BurdensCreek_20190801_20190902_PMGasMet.csv')
 se.Import_AirNowTech(airnowtech_path)
-"""
+
 
 # Mock evaluation using AIRS reference data downloaded from AirNowTech
-test = SensorEvaluation(
+Eval = SensorEvaluation(
                 sensor_name='Example_Make_Model',
                 eval_param='PM25',
                 reference_data=ref_path.as_posix() + '/airnowtech/processed',
@@ -46,8 +46,7 @@ test = SensorEvaluation(
                          '3': 'SN03'},
                 tzone_shift=5,
                 load_raw_data=False,
-                write_to_file=False)
-
+                write_to_file=True)
 
 #  ----------------------------------------------------------------------------
 # bbox for AIRS [set narrow margins (+/- 0.01 deg) around known coordinates]
@@ -56,8 +55,10 @@ AIRS_bbox = {"minLat": "35.88",
              "minLong": "-78.88",
              "maxLong": "-78.87"}
 
+SensorEvaluation.airnow_key = 'Your-AirNow-Key-Here'
+
 # Mock evaluation using AIRS reference data obtained from the AirNow API
-test = SensorEvaluation(sensor_name='Example_Make_Model',
+Eval = SensorEvaluation(sensor_name='Example_Make_Model',
                         eval_param='PM25',
                         reference_data='AirNow',
                         bbox=AIRS_bbox,
@@ -76,7 +77,10 @@ triple_oaks_ID = {"state": "37",
                   "county": "183",
                   "site": "0021"}
 
-test = SensorEvaluation(sensor_name='Example_Make_Model',
+SensorEvaluation.aqs_username = 'username_address@email.com'
+SensorEvaluation.aqs_key = 'Your-AQS-Key-Here'
+
+Eval = SensorEvaluation(sensor_name='Example_Make_Model',
                         eval_param='PM25',
                         reference_data='AQS',
                         aqs_id=triple_oaks_ID,
@@ -90,31 +94,31 @@ test = SensorEvaluation(sensor_name='Example_Make_Model',
 #  ----------------------------------------------------------------------------
 
 # Print performance target evaluation results to console
-test.print_eval_metrics(avg_interval='Hourly')
-test.print_eval_metrics(avg_interval='Daily')
+Eval.print_eval_metrics(avg_interval='Hourly')
+Eval.print_eval_metrics(avg_interval='Daily')
 
 # Print site testing conditions (concentration range, RH/temp range)
-test.print_eval_conditions(avg_interval='Hourly')
-test.print_eval_conditions(avg_interval='Daily')
+Eval.print_eval_conditions(avg_interval='Hourly')
+Eval.print_eval_conditions(avg_interval='Daily')
 
 # Timeseries plots for both 1-hour and 24-hour averaged data
-test.plot_timeseries(format_xaxis_weeks=False,
+Eval.plot_timeseries(format_xaxis_weeks=False,
                      yscale='linear',  # set y-axis format to linear scaling
                      date_interval=5,  # place 5 days between xticks
                      report_fmt=True)  # Reporting template formatting presets
 
 # Scatter plots for all sensors (sensor vs. ref), scatter are colored by RH
-test.plot_sensor_scatter('1-hour',
+Eval.plot_sensor_scatter('1-hour',
                          plot_limits=(-1, 20),
                          axes_spacing=5,
                          text_pos='upper_left')
 
 # Plot sensor base testing results for performance metrics and target ranges
-test.plot_metrics()
+Eval.plot_metrics()
 
 # Plot normalized sensor concentrations (sensor/reference) vs. temp and RH
-test.plot_met_influence(report_fmt=True,
+Eval.plot_met_influence(report_fmt=True,
                         plot_error_bars=False)
 
 # Plot the distribution of temp and RH observed durng the testing period
-test.plot_met_dist()
+Eval.plot_met_dist()
