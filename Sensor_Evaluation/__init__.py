99936e53f66b19720601557fb10a7dd5a8cbde2a
Samuel Frederick <frederick.samuel@epa.gov>
2021-07-29 08:58:28 -0400

    Update init with new plotting function scripts

--- Sensor_Evaluation/__init__.py
+++ Sensor_Evaluation/__init__.py
@@ -23,15 +23,17 @@ from ._analysis.uptime_calculator import Uptime_Calculator
 from ._analysis.deployment_period import Deployment_Period
 from ._analysis.duplicate_removal import Remove_Duplicates
 from ._analysis.normalize_calculator import Normalize
-from ._analysis.purpleair_modules import (Compute_AB_Averages,
-                                          USCorrection)
+from ._analysis.purpleair_modules import Compute_AB_Averages, USCorrection
 from ._format.format_date import Get_Date
-from ._format.format_names import (Format_Param_Name, Format_Metric_Name)
+from ._format.format_names import Format_Param_Name, Format_Metric_Name
 from ._format.sensor_subfolders import Create_Sensor_Directories
 from ._ingest.processed_data_loader import Processed_Data_Search
 from ._ingest.setup import Setup
 from ._ingest.standardized_ingest import Ingest
 from ._models.apply_correction import Individual_Correction
+from ._plotting.plot_formatting import (Set_Fontsize,
+     Sensor_Subplot_Formatting, Get_Max, Met_Scatter_Lims, Wrap_Text)
+from ._plotting.errorbars import Plot_Error_Bars
 from ._reference.import_airnowtech import Import_AirNowTech, Flatten
 from ._reference.load_ref_data import Load_Ref_DataFrames
 
@@ -42,20 +44,15 @@ from ._analysis.synoptic_idx import Synoptic_Index
 from ._analysis.concurr_deploy_grps import (Deployment_Groups,
                                             Reference_Stats,
                                             Meteorological_Stats)
-from ._analysis.cv_calculator import (CV_Calculator, Compute_CV)
+from ._analysis.cv_calculator import CV_Calculator, Compute_CV
 from ._models.sensor_ols import Sensor_OLS
-from ._analysis.model_analysis import (Regression_Stats,
-                                       Compute_RMSE)
-from ._plotting.plot_functions import (Set_Fontsize,
-                                       Draw_Scatter,
-                                       Sensor_Timeplot,
-                                       Scatter_Plotter,
-                                       Deployment_Timeline_Plot,
-                                       Stats_Comparison_Plot,
-                                       Ref_Dist_Plot,
-                                       Plot_Performance_Metrics,
-                                       Recording_Interval_Histogram,
-                                       Met_Distrib,
-                                       Normalized_Met_Scatter)
+from ._analysis.model_analysis import Regression_Stats, Compute_RMSE
+from ._plotting.distribution import (Ref_Dist_Plot, Met_Distrib,
+                                     Recording_Interval_Histogram)
+from ._plotting.intrasensor_comparison import Stats_Comparison_Plot
+from ._plotting.plot_performance_metrics import Plot_Performance_Metrics
+from ._plotting.timeseries import Sensor_Timeplot, Deployment_Timeline_Plot
+from ._plotting.scatter import (Draw_Scatter, Scatter_Plotter,
+                                Normalized_Met_Scatter)
 from ._analysis.intersensor_mean import Intersensor_Mean
 from ._ingest.sensor_import import Import
