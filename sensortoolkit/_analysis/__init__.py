86f7e8f0c6f61b4eb5b667209e4ca02d86711675
sfrede01 <frederick.samuel@epa.gov>
2021-08-19 09:03:04 -0400

    Add description to analysis init

---
+++ sensortoolkit/_analysis/__init__.py
@@ -0,0 +1,5 @@
+"""
+Analysis modules supporting the calculation of parameter quantities and
+statistical metrics that are either recommended by U.S. EPA's Performance
+Targets Reports, or may be used to compliment performance evaluation efforts.
+"""
