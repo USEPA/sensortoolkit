bd56b364d63922d490b44a1013500600f8c279d7
sfrede01 <frederick.samuel@epa.gov>
2021-08-24 17:06:15 -0400

    Add setup.py for making sensortoolkit package globally accessible on users system

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(name='sensortoolkit',
+      version='1.0.0',
+      description='Library for evaluting air sensor data',
+      author='Samuel Frederick, ORAU Contractor for U.S. EPA ORD',
+      author_email='frederick.samuel@epa.gov',
+      packages=['sensortoolkit',
+                'sensortoolkit/_analysis',
+                'sensortoolkit/_format',
+                'sensortoolkit/_ingest',
+                'sensortoolkit/_models',
+                'sensortoolkit/_plotting',
+                'sensortoolkit/_qc',
+                'sensortoolkit/_reference',
+                'sensortoolkit/_performancereport',
+                'sensortoolkit/_evaluation'
+                ]
+      )
