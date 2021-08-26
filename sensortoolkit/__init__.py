ad679792ff29e92b99801ffecf6f50c26f65b68c
sfrede01 <frederick.samuel@epa.gov>
2021-08-26 08:45:24 -0400

    Update naming for concurr_deploy_grps module and functions to create_deploy_dict

--- sensortoolkit/__init__.py
+++ sensortoolkit/__init__.py
@@ -36,7 +36,7 @@ from ._reference.ref_api_query import (Ref_API_Query, Save_Query,
                                        AQS_Query, AirNow_Query)
 from ._reference.import_oaqps import Ingest_OAQPS, Process_OAQPS
 from ._analysis.synoptic_idx import Synoptic_Index
-from ._analysis.concurr_deploy_grps import (Deployment_Groups,
+from ._analysis.create_deploy_dict import (Construct_Deploy_Dict,
                                             Reference_Stats,
                                             Meteorological_Stats)
 from ._analysis.cv_calculator import CV_Calculator, Compute_CV
