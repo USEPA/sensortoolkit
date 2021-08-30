6d97e76d18363224ea5eb1b8130d259be01ccb4a
sfrede01 <frederick.samuel@epa.gov>
2021-08-30 10:15:50 -0400

    Migrate setup from distutils to setuptools: makes use of pip's clean install/uninstall process. Also change version from generic '1.0.0' to '0.1.0bN' to indicate this is a beta testing version prior to inital public release. The 'long_description' parameter uses the readme.md file that is displayed on BitBucket, and I've added a license file for the standard open source MIT license. I've also included the reporting templates in the sensortoolkit package, so these are included as 'package_data' to ensure that they are included with the library.

--- setup.py
+++ setup.py
@@ -1,20 +1,51 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent
+readme = (here / 'README.md').read_text()
 
 setup(name='sensortoolkit',
-      version='1.0.0',
-      description='Library for evaluting air sensor data',
+      version='0.1.0b2',
+      description='Library for evaluating air sensor data',
+      long_description=readme,
+      long_description_content_type='text/markdown',
       author='Samuel Frederick, ORAU Contractor for U.S. EPA ORD',
       author_email='frederick.samuel@epa.gov',
-      packages=['sensortoolkit',
-                'sensortoolkit/_analysis',
-                'sensortoolkit/_format',
-                'sensortoolkit/_ingest',
-                'sensortoolkit/_models',
-                'sensortoolkit/_plotting',
-                'sensortoolkit/_qc',
-                'sensortoolkit/_reference',
-                'sensortoolkit/_performancereport',
-                'sensortoolkit/_evaluation',
-                'sensortoolkit/_pkg'
-                ]
+      license='The MIT License (MIT)',
+      license_files=('LICENSE.txt'),
+      classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        ],
+      packages=find_packages(include=['sensortoolkit',
+                'sensortoolkit._analysis',
+                'sensortoolkit._format',
+                'sensortoolkit._ingest',
+                'sensortoolkit._models',
+                'sensortoolkit._plotting',
+                'sensortoolkit._qc',
+                'sensortoolkit._reference',
+                'sensortoolkit._performancereport',
+                'sensortoolkit._evaluation',
+                'sensortoolkit._pkg'
+                ]),
+      package_data={'': ["_performancereport/templates/O3/Reporting_Template_Base_O3.pptx",
+                         "_performancereport/templates/PM25/Reporting_Template_Base_PM25.pptx"
+                         # data and figures/example_make_model placeholder image
+                         # method code lookup table
+                         ]
+                    },
+      install_requires=['matplotlib',
+                        'numpy',
+                        'pandas',
+                        'pathlib2',
+                        'python-pptx',
+                        'requests',
+                        'scipy',
+                        'seaborn',
+                        'statsmodels',
+                        'urllib3',
+                        'pip'
+                        ]
       )
