3336c1b32939a0f5ef86518c96e0feeae7dda4bb
sfrede01 <frederick.samuel@epa.gov>
2021-09-03 08:46:19 -0400

    Add _parameter subpackage. Add rst2pdf for potentially making a pdf version of the documentation, but there seem to be some bugs with the current version of rst2pdf, will check back in once they update the code to be compatible with Sphinx 4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,7 @@ sys.path.insert(0, os.path.abspath(lib + '/_evaluation'))
 sys.path.insert(0, os.path.abspath(lib + '/_format'))
 sys.path.insert(0, os.path.abspath(lib + '/_ingest'))
 sys.path.insert(0, os.path.abspath(lib + '/_models'))
+sys.path.insert(0, os.path.abspath(lib + '/_parameter'))
 sys.path.insert(0, os.path.abspath(lib + '/_performancereport'))
 sys.path.insert(0, os.path.abspath(lib +  '/_pkg'))
 sys.path.insert(0, os.path.abspath(lib + '/_plotting'))
@@ -37,8 +38,8 @@ author = 'Samuel Frederick'
 version = sensortoolkit.__version__
 release = version
 
-rst_epilog = '.. |sensortoolkit_version| replace:: ``%s``' % version
-
+rst_epilog = '.. |formatted_version| replace:: ``%s``' % version
+#rst_epilog = '.. |unformatted_version| replace:: %s' % version
 
 # -- General configuration ---------------------------------------------------
 
@@ -50,6 +51,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
+    'rst2pdf.pdfbuilder'
 ]
 
 
@@ -90,3 +92,81 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for PDF output --------------------------------------------------
+# Grouping the document tree into PDF files. List of tuples
+# (source start file, target name, title, author, options).
+#
+# If there is more than one author, separate them with \\.
+# For example: r'Guido van Rossum\\Fred L. Drake, Jr., editor'
+#
+# The options element is a dictionary that lets you override
+# this config per-document. For example:
+#
+# ('index', 'MyProject', 'My Project', 'Author Name', {'pdf_compressed': True})
+#
+# would mean that specific document would be compressed
+# regardless of the global 'pdf_compressed' setting.
+pdf_documents = [
+ ('index', 'sensortoolkit', 'sensortoolkit Documentation', 'Samuel Frederick'),
+]
+# A comma-separated list of custom stylesheets. Example:
+pdf_stylesheets = ['sphinx', 'kerning', 'a4']
+# A list of folders to search for stylesheets. Example:
+pdf_style_path = ['.', '_styles']
+# Create a compressed PDF
+# Use True/False or 1/0
+# Example: compressed=True
+# pdf_compressed = False
+# A colon-separated list of folders to search for fonts. Example:
+# pdf_font_path = ['/usr/share/fonts', '/usr/share/texmf-dist/fonts/']
+# Language to be used for hyphenation support
+# pdf_language = "en_US"
+
+# Mode for literal blocks wider than the frame. Can be
+# overflow, shrink or truncate
+# pdf_fit_mode = "shrink"
+# Section level that forces a break page.
+# For example: 1 means top-level sections start in a new page
+# 0 means disabled
+# pdf_break_level = 0
+# When a section starts in a new page, force it to be 'even', 'odd',
+# or just use 'any'
+# pdf_breakside = 'any'
+# Insert footnotes where they are defined instead of
+# at the end.
+# pdf_inline_footnotes = True
+# verbosity level. 0 1 or 2
+# pdf_verbosity = 0
+# If false, no index is generated.
+# NOTE: Open issue with rst2pdf "AttributeError: 'BuildEnvironment' object has no attribute 'indexentries'""
+# Users recommend setting index to false as the outdated code is called during index generation
+# Ticket: https://github.com/rst2pdf/rst2pdf/issues/1014
+pdf_use_index = False
+# If false, no modindex is generated.
+# pdf_use_modindex = True
+# If false, no coverpage is generated.
+# pdf_use_coverpage = True
+# Name of the cover page template to use
+# pdf_cover_template = 'sphinxcover.tmpl'
+# Documents to append as an appendix to all manuals.
+# pdf_appendices = []
+# Enable experimental feature to split table cells. Use it
+# if you get "DelayedTable too big" errors
+# pdf_splittables = False
+# Set the default DPI for images
+# pdf_default_dpi = 72
+# Enable rst2pdf extension modules
+# pdf_extensions = []
+# Page template name for "regular" pages
+# pdf_page_template = 'cutePage'
+# Show Table Of Contents at the beginning?
+# pdf_use_toc = True
+# How many levels deep should the table of contents be?
+#pdf_toc_depth = 9999
+# Add section number to section references
+#pdf_use_numbered_links = False
+# Background images fitting mode
+#pdf_fit_background_mode = 'scale'
+# Repeat table header on tables that cross a page boundary?
+#pdf_repeat_table_rows = True
