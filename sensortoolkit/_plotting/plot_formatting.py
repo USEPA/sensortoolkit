fefbbdb861f87b78a677211c98a13c598c839324
sfrede01 <frederick.samuel@epa.gov>
2021-09-03 08:20:31 -0400

    Add docstring for Get_Max, set param as only positional argument, dataframes as optional. Raise typerror if both dataframe objects (sensor and reference) are none. Add functionality to compute max of only sensor dataframe list if no ref dataframe passed to function.

--- sensortoolkit/_plotting/plot_formatting.py
+++ sensortoolkit/_plotting/plot_formatting.py
@@ -91,13 +91,43 @@ def Subplot_Dims(n_sensors):
     return (n_rows, n_cols)
 
 
-def Get_Max(df_list, ref_df, param, start=None, end=None):
-    # Determine maximum concentration recorded during timeframe, use to set
-    # default plot limits
+def Get_Max(param, df_list=None, ref_df=None, start=None, end=None):
+    """Determine maximum concentration measured across passed dataframes.
+
+    If both sensor dataframes are passed to ``df_list`` and a reference
+    dataframe is passed to ``ref_df``, the maximum will be computed across
+    both sensor and reference concentrations.
+
+    Args:
+        param (str): The name of the evaluation parameter.
+        df_list (list of pandas dataframes, optional): A list of sensor
+            dataframes. Defaults to None.
+        ref_df (pandas dataframe, optional): Reference dataframe. Defaults to
+            None. If dataframe passed, will be considered in calculation of
+            maximum concentration.
+        start (str, optional): The starting timestamp to begin search. Defaults
+            to None, will use the earliest timestamp recorded in datasets.
+        end (str, optional): The ending timestamp to end search. Defaults
+            to None, will use the latest timestamp recorded in datasets.
+
+    Returns:
+        max_conc (float): The maximum concentration indicated by the dataframes
+        passed to the function for the specified parameter.
+
+    Raises:
+        TypeError: If `df_list` and `ref_df` are both ``None`` (i.e., no
+            dataframes passed to function).
+
+    """
+    if df_list is None and ref_df is None:
+        raise TypeError('Get_Max() missing required dataframe objects: '
+                        '"df_list" and/or "ref_df"')
+
     max_list = [df.loc[start:end, param].max() for df in df_list]
 
-    ref_max = ref_df.loc[start:end, param + '_Value'].max()
-    max_list.append(ref_max)
+    if ref_df is not None:
+        ref_max = ref_df.loc[start:end, param + '_Value'].max()
+        max_list.append(ref_max)
 
     # Remove nans
     max_list = [i for i in max_list if not np.isnan(i)]
