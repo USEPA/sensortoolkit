0dc960b898a94d752a3cd2a34ed898d7c1b05693
sfrede01 <frederick.samuel@epa.gov>
2021-09-10 16:22:53 -0400

    Add module for converting temperature from one unit system to another. May use later with AQS data (temp tends to be in F via AQS)

---
+++ sensortoolkit/calculate/_convert_temp.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+@Author:
+  Samuel Frederick, NSSC Contractor (ORAU)
+  U.S. EPA, Office of Research and Development
+  Center for Environmental Measurement and Modeling
+  Air Methods and Characterization Division, Source and Fine Scale Branch
+  109 T.W Alexander Drive, Research Triangle Park, NC 27711
+  Office: 919-541-4086 | Email: frederick.samuel@epa.gov
+
+Created:
+  Fri Sep 10 09:11:01 2021
+Last Updated:
+  Fri Sep 10 09:11:01 2021
+"""
+import pandas as pd
+
+def convert_temp(data, from_unit='F', to_unit='C'):
+    # Convert input type to pandas dataframe
+    data_type = type(data)
+    if data_type is not pd.core.frame.DataFrame:
+        data = pd.Series(data).to_frame(name='Temp')
+
+    # Passed datatype is pandas dataframe but expected header not found
+    if 'Temp' not in data:
+        raise KeyError('Column header "Temp" not in passed dataframe.')
+
+    f_to_c = lambda x: (x - 32.0) / 1.8
+    c_to_f = lambda x: 1.8*(x) + 32.0
+
+    if from_unit == 'F' and to_unit == 'C':
+        print('..converting from Fahrenheit to Celsius')
+        converted_data = f_to_c(data)
+    elif from_unit == 'C' and to_unit == 'F':
+        print('..converting from Celsius to Fahrenheit')
+        converted_data = c_to_f(data)
+    else:
+        raise AttributeError('Invalid conversion. Arguments for "from_unit"'
+                             ' and "to_unit" must be either "C" or "F" and'
+                             ' should not be the same unit.')
+
+    return converted_data
