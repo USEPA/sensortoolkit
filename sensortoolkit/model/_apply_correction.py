a8676aadbdd64e222964774ff60f702031d231a2
sfrede01 <frederick.samuel@epa.gov>
2021-09-13 14:37:52 -0400

    Simplify author docstring

--- sensortoolkit/model/_apply_correction.py
+++ sensortoolkit/model/_apply_correction.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 @Author:
-  Samuel Frederick, NSSC Contractor (ORAU)
-  U.S. EPA, Office of Research and Development
-  Center for Environmental Measurement and Modeling
-  Air Methods and Characterization Division, Source and Fine Scale Branch
-  109 T.W Alexander Drive, Research Triangle Park, NC 27711
-  Office: 919-541-4086 | Email: frederick.samuel@epa.gov
-
+    | Samuel Frederick, NSSC Contractor (ORAU)
+    | U.S. EPA / ORD / CEMM / AMCD / SFSB
+    
 Created:
   Fri Oct 30 08:53:18 2020
 Last Updated:
--- sensortoolkit/model/_sensor_ols.py
+++ sensortoolkit/model/_sensor_ols.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 @Author:
-  Samuel Frederick, NSSC Contractor (ORAU)
-  U.S. EPA, Office of Research and Development
-  Center for Environmental Measurement and Modeling
-  Air Methods and Characterization Division, Source and Fine Scale Branch
-  109 T.W Alexander Drive, Research Triangle Park, NC 27711
-  Office: 919-541-4086 | Email: frederick.samuel@epa.gov
+    | Samuel Frederick, NSSC Contractor (ORAU)
+    | U.S. EPA / ORD / CEMM / AMCD / SFSB
 
 Created:
   Mon Jul 27 09:21:14 2020
