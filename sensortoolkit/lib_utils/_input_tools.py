e439a686e2c4e73d1b5819f2a57ffd21ac911493
sfrede01 <frederick.samuel@epa.gov>
2021-09-23 12:06:21 -0400

    Add an indent statement argument for optionally indenting console messages, helpful for setup process when a indented entry sequence is visually helpful

--- sensortoolkit/lib_utils/_input_tools.py
+++ sensortoolkit/lib_utils/_input_tools.py
@@ -14,20 +14,22 @@ Last Updated:
   Mon Sep 20 15:13:36 2021
 """
 
-def validate_entry():
+def validate_entry(indent_statement=0):
     val = ''
+    indent = ' '*indent_statement
     options = ['y', 'n']
     while val not in options:
-        val = input('Confirm entry [y/n]: ')
+        val = input(f'{indent}Confirm entry [y/n]: ')
         if val in options:
             return val
         else:
-            print('..invalid entry, select [y/n]')
+            print(f'{indent}..invalid entry, select [y/n]')
 
-def enter_continue():
+def enter_continue(indent_statement=0):
+    indent = ' '*indent_statement
     end = False
     while end is False:
-        return_val = input('Press enter to continue.')
+        return_val = input(f'{indent}Press enter to continue.')
         if return_val == '':
             end=True
     print('')
