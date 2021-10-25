e63c51614784f16c136b134e3e1245dedfaf6533
sfrede01 <frederick.samuel@epa.gov>
2021-10-25 16:08:40 -0400

    Update docs build

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.2b4',
+    VERSION: '0.5.6b2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
