cb4eb676005e7ed46403ab88f69925724b984a32
sfrede01 <frederick.samuel@epa.gov>
2022-02-09 16:05:27 -0500

    Remove custom ingestion functions from sensor_import.py in favor of passing function objects to AirSensor.load_data() for utilizing custom ingestion modules instead of the standard_ingest() method.

--- sensortoolkit/ingest/_sensor_import.py
+++ sensortoolkit/ingest/_sensor_import.py
@@ -347,407 +347,53 @@ def ingest_wrapper(cwd, sensor_name, serial, data_path, custom_ingest):
         return standard_ingest(cwd, name=sensor_name,
                                setup_file_path=setup_path)
     else:
-        return globals()[custom_ingest](cwd, serial)
-        # Otherwise, use sensor-specific ingestion modules
-        # if 'purpleair' in sensor_name.lower():
-        #     # assuming Thingspeak API dataset
-        #     return ingest_purpleair(cwd, serial)
-
-    #    if sensor_name == 'Your_Sensor_Model_Here':
-    #        return Custom_Ingest_Module_For_Your_Sensor(cwd)
-
-        # else:
-        #     sys.exit('No sensor specific import module specified for '
-        #              + sensor_name)
+        return custom_ingest(cwd, serial)
 
 
-# Sensor specific ingestion modules
+# [DEPRECIATED] Sensor specific ingestion modules
 
-# Depreciate custom ingestion module for example dataset
-# def ingest_example_make_model(cwd):
-#     """Ingestion module for an example sensor dataset.
-
-#     Recorded sensor data are imported and headers are converted into a
-#     standard format for analysis.
-
-#     Args:
-#         cwd (str): The full path to the sensor data file
-
-#     Returns:
-#         df (Pandas DataFrame object): A dataframe containing sensor data that
-#             has been converted into standardized syntax
+# def custom_ingest_module_for_your_sensor(cwd):
+#     """Ingestion module for converting your sensor data to standard format.
+#     Below are some useful guidelines for setting up your sensor's data
+#     ingestion module.
 #     """
-#     idx_name = 'Time'
+#     # The name of the column you'd like to set as the time-like index
+#     idx_name = 'Timestamp_column'
 
-#     try:
-#         df = pd.read_csv(cwd, header=5, index_col=idx_name,
-#                          parse_dates=[idx_name])
-#     except FileNotFoundError as e:
-#         sys.exit(e)
-
-#     df.index.name = 'DateTime'
-
-#     # Force non numeric values to Nans
-#     df = df.apply(lambda x: pd.to_numeric(x, errors='coerce'))
-
-#     # Drop unsed columns and rename others to consistent naming scheme
-#     df = df.drop('Inlet', axis=1)
-#     df = df.rename(columns={'NO2 (ppb)': 'NO2',
-#                             'O3 (ppb)': 'O3',
-#                             'PM2.5 (µg/m³)': 'PM25',
-#                             'TEMP (°C)': 'Temp',
-#                             'RH (%)': 'RH',
-#                             'DP (°C)': 'DP'})
-#     return df
-
-
-# def ingest_sensit_ramp(cwd):
-#     """Ingestion module for the Sensit RAMP.
-
-#     Args:
-#         cwd (str): The full path to the sensor data file
-
-#     Returns:
-#         df (Pandas DataFrame object): A dataframe containing sensor data that
-#             has been converted into standardized syntax
-#     """
-
-#     # List of column names
-#     col_list = ['Serial_ID', 'DateTime', 'CO_Header', 'CO',
-#                 'NO_Header', 'NO', 'NO2_Header', 'NO2',
-#                 'O3_Header', 'O3', 'CO2_Header', 'CO2',
-#                 'Temp_Header', 'Temp', 'RH_Header', 'RH',
-#                 'PM1_Header', 'PM1', 'PM25_Header', 'PM25',
-#                 'PM10_Header', 'PM10', 'WD_Header', 'WD',
-#                 'WS_Header', 'WS', 'BATT_Header', 'BATT',
-#                 'CHRG_Header', 'CHRG', 'RUN_Header', 'RUN',
-#                 'SD_Header', 'SD', 'RAW_Header', 'RAW_1',
-#                 'RAW_2', 'RAW_3', 'RAW_4', 'RAW_5', 'RAW_6',
-#                 'RAW_7', 'RAW_8', 'STAT_Header', 'STAT_1',
-#                 'STAT_2', 'STAT_3']
-
-#     # Load sensor datasets, column names not specified in files (header==None)
-#     try:
-#         df = pd.read_csv(cwd, header=None, names=col_list)
-#     except FileNotFoundError as e:
-#         sys.exit(e)
-
-
-#     # drop the header columns for RAMP datasets
-#     drop_headers = [col for col in df.columns if col.endswith('_Header')]
-#     df = df.drop(columns=drop_headers)
-
-#     df.Serial_ID = df.Serial_ID.str.replace('XDATE', '')
-
-#     # Set the DateTime columns as the timelike index
-#     df = df.set_index(pd.to_datetime(df['DateTime']))
-#     df.index.name = 'DateTime'
-
-#     # Limit dataset to parameter data (exclude columns not used in analysis)
-#     df = df[['Serial_ID', 'CO', 'NO', 'NO2', 'O3', 'CO2',
-#              'PM1', 'PM25', 'PM10', 'Temp', 'RH', 'WD', 'WS']]
-
-#     return df
-
-
-def pa_thingspeak(cwd, serial):
-    """Ingestion module for the PurpleAir PA-II (and PA-II-SD).
-
-    Args:
-        cwd (str):
-            The full path to the sensor data file.
-        serial (str):
-            The serial identifier unique to each sensor unit.
-
-    Returns:
-        df (Pandas DataFrame object):
-            A dataframe containing sensor data that has been converted into
-            SDFS format.
-
-    .. important::
-
-      PurpleAir data formatting vary depending on the source of acquisition
-      and this module is intended *ONLY* for data downloaded from the
-      Thingspeak API.
-
-    The Import() module expects a single data frame for each sensor (sensor_df)
-    for use in analysis. Because the PurpleAir PAII contains two internal PM
-    sensors corresponding to data channels 'A' and 'B', this ingestion module
-    expects data for these seperate channels to be in two unique files.
-
-    Data files for each sensor channel MUST contain the sensor serial ID
-    followed immediately by an upper-case 'A' or 'B' depending on the channel
-    (e.g., PurpleAir sensor with serial ID de90 must have two associated .csv
-    files that contain the phrases 'de90A' for channel A data or 'de90B' for
-    channel B data).
-
-    Data for channels A and B are loaded in tandem based on the naming scheme
-    for the channel A file (note file names can differ only by the distinction
-    of 'A' or 'B' channel data). These data sets are then merged and the
-    QC criteria of Barkjohn et al. 2021 are used to computing AB averages.
-    Finally, the US-wide correction equation developed by Barkjohn et al. 2021
-    is computed for the AB averages and added as an additional column to the
-    merged dataset.
-
-    """
-    if serial+'A' in cwd:
-        # Load data for both channels A and B
-        for channel in ['A', 'B']:
-            if channel == 'B':
-                cwd = cwd.replace(serial+'A', serial+'B')
-
-            try:
-                df = pd.read_csv(cwd, encoding='utf-16')
-            except FileNotFoundError as e:
-                sys.exit(e)
-
-            df = df.rename(columns={'monitor-id': 'Sensor_id',
-                                    ' channel-id': 'Channel',
-                                    ' created-at': 'DateTime',
-                                    ' entry-id': 'Entry',
-                                    ' PM1.0 (ATM)': 'PM1_ATM',
-                                    ' PM2.5 (ATM)': 'PM25_ATM',
-                                    ' PM10.0 (ATM)': 'PM10_ATM',
-                                    ' Uptime': 'Uptime',
-                                    ' RSSI': 'RSSI',
-                                    ' Temperature': 'Temp',
-                                    ' Humidity': 'RH',
-                                    ' PM2.5 (CF=1)': 'PM25_CF1',
-                                    ' Mem': 'Mem',
-                                    ' Adc': 'Adc',
-                                    ' Pressure': 'Press',
-                                    ' Unused': 'Unused'})
-
-            df.DateTime = df.DateTime.str.replace(
-                                'T', ' ').str.replace('Z', '').str.lstrip(' ')
-
-            timestamp_fmt = '%Y-%m-%d %H:%M:%S'
-            df = df.set_index(pd.to_datetime(df['DateTime'],
-                                             format=timestamp_fmt)
-                              ).sort_index()
-            df = df.tz_localize('UTC')
-
-            df = df.drop(['Sensor_id',
-                          'Channel',
-                          'DateTime',
-                          'Entry'], axis=1)
-
-            if channel == 'A':
-                df_A = df.drop(['RSSI', 'Uptime'], axis=1)
-            if channel == 'B':
-                df_B = df.drop(['Mem', 'Adc', 'Unused'], axis=1)
-
-        # Merge the channel dataframes
-        df = pd.merge_asof(df_A, df_B, on='DateTime',
-                           suffixes=['_a', '_b'], #left_index=True,
-                           tolerance=pd.Timedelta('5s'),
-                           direction='nearest')
-        df = df.set_index('DateTime', drop=True)
-
-        df = df.rename(columns={'Temp': 'Temp_Value',
-                                'RH': 'RH_Value',
-                                'Press': 'Press_Value'})
-        # Convert Temp from F to C
-        df['Temp_Value'] = (df['Temp_Value'] - 32) / 1.8
-
-        return df
-
-    if serial + 'B' in cwd:
-        return None
-
-def pa_sdcard(cwd, serial):
-    """
-
-    Note, this assume preprocessing of datasets:
-    - Timestamp column has been changed from 'UTCDateTime' to 'DateTime'
-    - Rows where an invalid timestamp entry was encountered (bad formatting)
-      were dropped from the preprocessed dataset.
-
-    Args:
-        cwd (TYPE): DESCRIPTION.
-        serial (TYPE): DESCRIPTION.
-
-    Returns:
-        df (TYPE): DESCRIPTION.
-
-    """
-
-    df = pd.read_csv(cwd, parse_dates=['DateTime'], index_col='DateTime')
-
-    file_versions = list(df.firmware_ver.unique())
-
-    if 4.02 in file_versions:
-        print('......version 4.02 detected')
-
-        firmware_df = df[df.firmware_ver==4.02]
-
-        firmware_df = firmware_df.rename(
-            columns={'hardwareversion,': 'pm1_0_atm,',
-                     'pm1_0_atm,': 'pm2_5_atm,',
-                     'pm2_5_atm,': 'pm10_0_atm,',
-                     'pm10_0_atm,': 'pm1_0_cf_1,',
-                     'pm1_0_cf_1,': 'pm2_5_cf_1,',
-                     'pm2_5_cf_1,': 'pm10_0_cf_1,',
-                     'pm10_0_cf_1,': 'pm2.5_aqi_atm,',
-                     'pm2.5_aqi_atm,': 'pm2.5_aqi_cf_1,',
-                     'pm2.5_aqi_cf_1,': 'p_0_3_um,',
-                     'p_0_3_um,': 'p_0_5_um,',
-                     'p_0_5_um,': 'p_1_0_um,',
-                     'p_1_0_um,': 'p_2_5_um,',
-                     'p_2_5_um,': 'p_5_0_um,',
-                     'p_5_0_um,': 'p_10_0_um,',
-                     'p_10_0_um,': 'pm1_0_atm_b,',
-                     'pm1_0_atm_b,': 'pm2_5_atm_b,',
-                     'pm2_5_atm_b,': 'pm10_0_atm_b,',
-                     'pm10_0_atm_b,': 'pm1_0_cf_1_b,',
-                     'pm1_0_cf_1_b,': 'pm2_5_cf_1_b,',
-                     'pm2_5_cf_1_b,': 'pm10_0_cf_1_b,',
-                     'pm10_0_cf_1_b,': 'pm2.5_aqi_atm_b,',
-                     'pm2.5_aqi_atm_b,': 'pm2.5_aqi_cf_1_b,',
-                     'pm2.5_aqi_cf_1_b,': 'p_0_3_um_b,',
-                     'p_0_3_um_b,': 'p_0_5_um_b,',
-                     'p_0_5_um_b,': 'p_1_0_um_b,',
-                     'p_1_0_um_b,': 'p_2_5_um_b,',
-                     'p_2_5_um_b,': 'p_5_0_um_b,',
-                     'p_5_0_um_b,': 'p_10_0_um_b,',
-                     'p_10_0_um_b,': 'gas'})
-        firmware_df_idx = firmware_df.index
-        firmware_df = firmware_df.drop(columns=['gas'])
-
-        df.loc[firmware_df_idx, :] = firmware_df
-
-    # Firmware prior to v5.00 had the CF=1 and CF=ATM labels swapped.
-    version_label_swap = [version for version in file_versions if version < 5.00]
-
-    # Correct labels for older firmware versions
-    if version_label_swap != []:
-        for version in version_label_swap:
-            print('......swapping labels')
-            firmware_df = df[df.firmware_ver==version]
-
-            rename = {'pm1_0_atm': 'pm1_0_cf_1',
-                     'pm2_5_atm': 'pm2_5_cf_1',
-                     'pm10_0_atm': 'pm10_0_cf_1',
-                     'pm1_0_cf_1': 'pm1_0_atm',
-                     'pm2_5_cf_1': 'pm2_5_atm',
-                     'pm10_0_cf_1': 'pm10_0_atm',
-                     'pm2.5_aqi_atm': 'pm2.5_aqi_cf_1',
-                     'pm2.5_aqi_cf_1': 'pm2.5_aqi_atm',
-                     'pm1_0_atm_b': 'pm1_0_cf_1_b',
-                     'pm2_5_atm_b': 'pm2_5_cf_1_b',
-                     'pm10_0_atm_b': 'pm10_0_cf_1_b',
-                     'pm1_0_cf_1_b': 'pm1_0_atm_b',
-                     'pm2_5_cf_1_b': 'pm2_5_atm_b',
-                     'pm10_0_cf_1_b': 'pm10_0_atm_b',
-                     'pm2.5_aqi_atm_b': 'pm2.5_aqi_cf_1_b',
-                     'pm2.5_aqi_cf_1_b': 'pm2.5_aqi_atm_b'}
-
-            # Rename old column names with new names
-            firmware_df = firmware_df.rename(
-                columns={**rename, **{val:key for key, val in rename.items()}})
-
-
-            firmware_df_idx = firmware_df.index
-
-            df.loc[firmware_df_idx, :] = firmware_df
-
-    drop_cols = ['mac_address',
-                #'firmware_ver',  # Keeping firmware version in for time being
-                'hardware',
-                'adc',
-                'mem',
-                'rssi',
-                'uptime',
-                'p_0_3_um',
-                'p_0_5_um',
-                'p_1_0_um',
-                'p_2_5_um',
-                'p_5_0_um',
-                'p_10_0_um',
-                'p_0_3_um_b',
-                'p_0_5_um_b',
-                'p_1_0_um_b',
-                'p_2_5_um_b',
-                'p_5_0_um_b',
-                'p_10_0_um_b',
-                'hardwareversion',
-                'pm2.5_aqi_atm',
-                'pm2.5_aqi_cf_1',
-                'pm2.5_aqi_atm_b',
-                'pm2.5_aqi_cf_1_b',
-                'gas']
-
-    drop = [i for i in drop_cols if i in df.columns]
-
-    df = df.drop(columns=drop)
-
-    df = df.rename(columns={'current_temp_f': 'Temp_Value',
-                            'current_humidity': 'RH_Value',
-                            'current_dewpoint_f': 'DP_Value',
-                            'pressure': 'Press_Value',
-                            'pm1_0_atm': 'PM1_ATM_A_Value',
-                            'pm2_5_atm': 'PM25_ATM_A_Value',
-                            'pm10_0_atm': 'PM10_ATM_A_Value',
-                            'pm1_0_cf_1': 'PM1_CF1_A_Value',
-                            'pm2_5_cf_1': 'PM25_CF1_A_Value',
-                            'pm10_0_cf_1': 'PM10_CF1_A_Value',
-                            'pm1_0_atm_b': 'PM1_ATM_B_Value',
-                            'pm2_5_atm_b': 'PM25_ATM_B_Value',
-                            'pm10_0_atm_b': 'PM10_ATM_B_Value',
-                            'pm1_0_cf_1_b': 'PM1_CF1_B_Value',
-                            'pm2_5_cf_1_b': 'PM25_CF1_B_Value',
-                            'pm10_0_cf_1_b': 'PM10_CF1_B_Value'
-                            })
-
-    # Convert F to C
-    df.Temp_Value = convert_temp(df.Temp_Value, verbose=False)
-    df.DP_Value = convert_temp(df.DP_Value, verbose=False)
-
-    return df
-
-
-#def custom_ingest_module_for_your_sensor(cwd):
-#    """Ingestion module for converting your sensor data to standard format.
-#    Below are some useful guidelines for setting up your sensor's data
-#    ingestion module.
-#    """
-#    # The name of the column you'd like to set as the time-like index
-#    idx_name = 'Timestamp_column'
-#
-#    # Read in csv files at the current working directory 'cwd'
-#    df = pd.read_csv(cwd,
+#     # Read in csv files at the current working directory 'cwd'
+#     df = pd.read_csv(cwd,
 #                     header=0, # Modify if data don't start on the first row
-#
+
 #                     # These last two columns are useful if you know there is
 #                     # a particular column with timestamps to set as the index
 #                     index_col=idx_name,
 #                     parse_dates=[idx_name])
-#
-#    # If the time-like index should to be a combination of columns, (e.g., if
-#    # one column has date information and another has time information) this
-#    # can be accomplished via some code that looks something like:
-#    # df['timestamp_column'] = df['column_A'] + df['column_B']
-#
-#    # Reset the index name to DateTime. If the timestamp is not in UTC,
-#    # you can use something like df.shift(N, freq='H') where N is the number of
-#    # hours you want to shift by.
-#    df.index.name = 'DateTime'  # Don't change this line
-#
-#    # Force non numeric values to Nans
-#    # Optional, however this may remove some useful data like QC flags or
-#    # status codes. Sometimes this is helpful for setting a consistent numeric
-#    # data type for the data in each column to avoid type/value error.
-#    df = df.apply(lambda x: pd.to_numeric(x, errors='coerce'))
-#
-#    # Drop unsed columns and rename others to consistent naming scheme
-#    df = df.drop(['Columns', 'you', 'want', 'to', 'drop'] , axis=1)
-#    df = df.rename(
-#        # Below are some examples of sensor data columns to rename
-#        # The list and number of parameters will change depending on your sensor
-#        columns={'The label for NO2 in your sensor data files': 'NO2',
+
+#     # If the time-like index should to be a combination of columns, (e.g., if
+#     # one column has date information and another has time information) this
+#     # can be accomplished via some code that looks something like:
+#     # df['timestamp_column'] = df['column_A'] + df['column_B']
+
+#     # Reset the index name to DateTime. If the timestamp is not in UTC,
+#     # you can use something like df.shift(N, freq='H') where N is the number of
+#     # hours you want to shift by.
+#     df.index.name = 'DateTime'  # Don't change this line
+
+#     # Force non numeric values to Nans
+#     # Optional, however this may remove some useful data like QC flags or
+#     # status codes. Sometimes this is helpful for setting a consistent numeric
+#     # data type for the data in each column to avoid type/value error.
+#     df = df.apply(lambda x: pd.to_numeric(x, errors='coerce'))
+
+#     # Drop unsed columns and rename others to consistent naming scheme
+#     df = df.drop(['Columns', 'you', 'want', 'to', 'drop'] , axis=1)
+#     df = df.rename(
+#         # Below are some examples of sensor data columns to rename
+#         # The list and number of parameters will change depending on your sensor
+#         columns={'The label for NO2 in your sensor data files': 'NO2',
 #                 'The label for O3 in your sensor data files': 'O3',
 #                 'The label for PM2.5 in your sensor data files': 'PM25',
 #                 'The label for temperature in your sensor data files': 'Temp',
 #                 'The label for relative humidity in your sensor data files': 'RH',
 #                 'The label for dewpoint in your sensor data files': 'DP'})
-#    return df
+#     return df
