74726a7ab6f22ceac2632922daa6ffe04c32caf5
sfrede01 <samfred12@gmail.com>
2022-07-01 12:34:31 -0400

    Accelerate using apply rather than for loop for stepping through each warm up event

--- sensortoolkit/qc/_detect_warmup_events.py
+++ sensortoolkit/qc/_detect_warmup_events.py
@@ -60,24 +60,34 @@ def warmup_incidents(data, param, interval=None, warmup_duration=None,
 
     data = data.set_index(data.DateTime)
 
-    invalidate_duration = warmup_duration
-    for grp in data.value_grp.unique():
 
-        grp_data = data[data.value_grp==grp]
-        grp_data[f'{param}_QAQC_Flag'] = 0
-        grp_start = grp_data.index.min()
-        invalidate_end = grp_start + pd.to_timedelta(invalidate_duration)
+    n_warmups = len(data.value_grp.dropna().unique())
+    print(f'{n_warmups} warmup events detected')
 
-        grp_data.loc[grp_start:invalidate_end, f'{param}_QAQC_Flag'] = 1
-        data.loc[grp_data.index, f'{param}_Value'] = grp_data[f'{param}_Value']
-        data.loc[grp_data.index, f'{param}_QAQC_Flag'] = grp_data[f'{param}_QAQC_Flag']
+    invalidate_duration = pd.to_timedelta(warmup_duration)
+    data = data.groupby('value_grp').apply(lambda x:
+                                            write_warmup_code(x, param,
+                                                              invalidate_duration))
 
     data = data.drop(columns=['DateTime', 'value_grp'])
 
     if invalidate:
-        invalid_idx = data[data[f'{param}_QAQC_Flag']==1].index
+        invalid_idx = data[data[f'{param}_QAQC_Code']=='WarmUp'].index
         data.loc[invalid_idx, f'{param}_Value'] = np.nan
 
-    passed_dataframe[[f'{param}_Value', f'{param}_QAQC_Flag']] = data
+    passed_dataframe[[f'{param}_Value', f'{param}_QAQC_Code']] = data
 
     return passed_dataframe
+
+
+def write_warmup_code(series, param, invalidate_duration):
+    i = series.value_grp.unique()[0]
+
+    if i % 1000 == 0:
+        print(f'..{i}')
+
+    idx_min = series.index.min()
+    idx_max = idx_min + invalidate_duration
+    series.loc[idx_min:idx_max, f'{param}_QAQC_Code'] = 'WarmUp'
+
+    return series
